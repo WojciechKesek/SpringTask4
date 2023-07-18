67160d94f2b987aa01e1ee729f9b13cdcef08a99
Wojciech Kesek <wojtekesek@gmail.com>
2023-07-18 18:41:44 +0200

    another solution

--- src/main/java/com/example/springtask4/command/CommandLineRunnerTest.java
+++ src/main/java/com/example/springtask4/command/CommandLineRunnerTest.java
@@ -1,25 +1,27 @@
 package com.example.springtask4.command;
 
-import com.example.springtask4.config.UtilConfiguration;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
-
 import java.util.Arrays;
+import java.util.Set;
 
 @Component
 @RequiredArgsConstructor
 public class CommandLineRunnerTest implements CommandLineRunner {
     private final ApplicationContext applicationContext;
+    private final Set<String> correctNames = Set.of("dummyLogger", "listUtility", "stringUtility");
     @Override
     public void run(String... args) throws Exception {
+        System.out.println("Solution 1");
         String[] beans = applicationContext.getBeanDefinitionNames();
         Arrays.stream(beans)
                 .filter(s-> s.equalsIgnoreCase("listUtility")
                 || s.equalsIgnoreCase("stringUtility")
                 || s.equalsIgnoreCase("dummyLogger"))
                 .forEach(System.out::println);
+        System.out.println("Solution 2");
+        System.out.println(Set.of(applicationContext.getBeanDefinitionNames()).containsAll(correctNames));
     }
 }
